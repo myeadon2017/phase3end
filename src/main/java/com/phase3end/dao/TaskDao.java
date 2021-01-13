ed756c0437abb76d121af51090cd53aa506433ca
Matthew Yeadon <matthewyeadon2020@gmail.com>
2021-01-13 11:20:40 -0500

    new commit

--- src/main/java/com/phase3end/dao/TaskDao.java
+++
@@ -1,9 +0,0 @@
-package com.phase3end.dao;
-
-import com.phase3end.model.Task;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaskDao extends CrudRepository<Task, Integer> {
-}
